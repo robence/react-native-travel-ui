b47c9167761cf3b24ccf951aefb324d92bef8774
Bence Rozsa <robence@gmail.com>
2020-10-04 09:41:43 +0200

    feat(ColoredCard): implement selection

--- src/components/Trip/ColoredCardList/ColoredCardList.tsx
+++ src/components/Trip/ColoredCardList/ColoredCardList.tsx
@@ -5,30 +5,47 @@ import ColoredCard from './ColoredCard';
 import { Budget, MidClass, Luxury } from '../../../components/svg';
 import { Label } from '../../UI';
 import { Colors } from '../../../constants';
+import type { SelectHandlerProps } from '../../headless/SelectHandler';
 
-export default function ColoredCardList() {
+const cardConfig = [
+  {
+    label: 'Budget',
+    color: Colors.red,
+    backgroundColor: Colors.lightRed,
+    Icon: Budget,
+  },
+  {
+    label: 'Mid-class',
+    color: Colors.yellow,
+    backgroundColor: Colors.lightYellow,
+    Icon: MidClass,
+  },
+  {
+    label: 'Luxury',
+    color: Colors.primary,
+    backgroundColor: Colors.lightBlue,
+    Icon: Luxury,
+  },
+];
+
+type ColoredCardListProps = SelectHandlerProps;
+
+export default function ColoredCardList(props: ColoredCardListProps) {
+  const { selected: selectedLabel, setSelected } = props;
+
   return (
     <>
       <Label>Travel style</Label>
       <View style={styles.inlineCards}>
-        <ColoredCard
-          label="Budget"
-          color={Colors.red}
-          backgroundColor={Colors.lightRed}
-          Icon={Budget}
-        />
-        <ColoredCard
-          label="Mid-class"
-          color={Colors.yellow}
-          backgroundColor={Colors.lightYellow}
-          Icon={MidClass}
-        />
-        <ColoredCard
-          label="Luxury"
-          color={Colors.primary}
-          backgroundColor={Colors.lightBlue}
-          Icon={Luxury}
-        />
+        {cardConfig.map(({ label, color, backgroundColor, Icon }) => (
+          <ColoredCard
+            key={label}
+            label={label}
+            selected={selectedLabel === label}
+            setSelected={() => setSelected(label)}
+            options={{ color, backgroundColor, Icon }}
+          />
+        ))}
       </View>
     </>
   );
